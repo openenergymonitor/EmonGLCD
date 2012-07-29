068f367e16aaee6ea6986c3672c5692102a74e7b
Trystan <trystan@trystan-laptop.(none)>
2012-07-29 10:32:48 +0100

    EmonGLCD Template based display sketch

---
+++ emonGLCD_Template_Dev/emonGLCD_Template_Dev.ino
@@ -0,0 +1,74 @@
+/*
+
+  EmonGLCD Template based display specification (in development)
+
+  Contributors: Glyn Hudson, Trystan Lea
+  Icons implementation by DrsDre
+  GLCD library by Jean-Claude Wippler: JeeLabs.org
+
+  Part of the: openenergymonitor.org project
+  Licenced under GNU GPL V3
+  
+*/ 
+
+#include <JeeLib.h>
+#include <GLCD_ST7565.h>
+#include <avr/pgmspace.h>
+GLCD_ST7565 glcd;
+
+unsigned long fast_update;
+
+// Fixed values for this example
+double temp = 17.4 ,maxtemp = 19 ,mintemp = 16;
+int hour = 23, minute = 43;
+double use = 252, usekwh = 2.5;
+double gen = 1760, genkwh = 4.8;
+
+void setup()
+{
+  glcd.begin(0x20);
+  glcd.backLight(200);
+}
+
+void loop()
+{
+  //--------------------------------------------------------------------------------------------
+  // Display update every 200ms
+  //--------------------------------------------------------------------------------------------
+  if ((millis()-fast_update)>200)
+  {
+    fast_update = millis();
+    
+    int S1 = digitalRead(15);
+    int S2 = digitalRead(16);
+    int S3 = digitalRead(19);
+
+    if (S1==1)
+    {
+      // powerstr, power, energystr, kwh
+      draw_power_page( "POWER" ,use, "USE", usekwh);
+
+      // temp, mintemp, maxtemp, hour, minute
+      draw_temperature_time_footer(temp, mintemp, maxtemp, hour,minute);
+      
+      glcd.refresh();
+    }
+    else if (S2==1)
+    {
+      // powerstr, power, energystr, kwh
+      draw_power_page( "SOLAR" ,gen, "GEN", genkwh);
+
+      // temp, mintemp, maxtemp, hour, minute
+      draw_temperature_time_footer(temp, mintemp, maxtemp, hour,minute);
+      
+      glcd.refresh();
+    }
+    else
+    {
+      // use, usekwh, gen, maxgen, genkwh, temp, mintemp, maxtemp, hour, minute
+      draw_solar_page(use, usekwh, gen, 2050, genkwh, temp, mintemp, maxtemp, 12,43);
+      glcd.refresh();
+    }
+    
+  } 
+}
---
+++ emonGLCD_Template_Dev/templates.ino
@@ -0,0 +1,216 @@
+#include "utility/font_helvB24.h"
+#include "utility/font_helvB14.h"
+#include "utility/font_helvB12.h"
+#include "utility/font_clR4x6.h"
+#include "utility/font_clR6x8.h"
+
+//------------------------------------------------------------------
+// Draws a page showing a single power and energy value in big font
+//------------------------------------------------------------------
+void draw_power_page(char* powerstr, double powerval, char* energystr,  double energyval)
+{ 
+  glcd.clear();
+  glcd.fillRect(0,0,128,64,0);
+  
+  char str[50];    			 //variable to store conversion 
+  glcd.setFont(font_clR6x8);      
+  strcpy(str,powerstr);  
+  strcat(str," NOW:"); 
+  glcd.drawString(0,0,str);
+  strcpy(str,energystr);  
+  strcat(str," TODAY:"); 
+  glcd.drawString(0,38,str);
+
+  // power value
+  glcd.setFont(font_helvB24);
+  itoa((int)powerval,str,10);
+  strcat(str,"w");   
+  glcd.drawString(3,9,str);
+  
+  // kwh per day value
+  glcd.setFont(font_clR6x8);
+  if (energyval<10.0) dtostrf(energyval,0,1,str); else itoa((int)energyval,str,10);
+  strcat(str,"kWh");
+  glcd.drawString(87,38,str);        
+}
+
+
+//------------------------------------------------------------------
+// Draws a footer showing GLCD temperature and the time
+//------------------------------------------------------------------
+void draw_temperature_time_footer(double temp, double mintemp, double maxtemp, double hour, double minute)
+{
+  glcd.drawLine(0, 47, 128, 47, WHITE);     //middle horizontal line 
+
+  char str[50];
+  // GLCD Temperature
+  glcd.setFont(font_helvB12);  
+  dtostrf(temp,0,1,str); 
+  strcat(str,"C");
+  glcd.drawString(0,50,str);  
+  
+  // Minimum and maximum GLCD temperature
+  glcd.setFont(font_clR4x6);             
+  itoa((int)mintemp,str,10);
+  strcat(str,"C");
+  glcd.drawString_P(46,51,PSTR("MIN"));
+  glcd.drawString(62,51,str);
+               
+  itoa((int)maxtemp,str,10); 
+  strcat(str,"C");
+  glcd.drawString_P(46,59,PSTR("MAX"));
+  glcd.drawString(62,59,str);
+  
+  // Time
+  char str2[5];
+  itoa((int)hour,str,10);
+  if  (minute<10) strcat(str,":0"); else strcat(str,":");
+  itoa((int)minute,str2,10);
+  strcat(str,str2); 
+  glcd.setFont(font_helvB12);
+  glcd.drawString(88,50,str);
+
+}
+
+//------------------------------------------------------------------
+// Draws the Solar import/export page
+//------------------------------------------------------------------
+void draw_solar_page(double use, double usekwh, double gen, double maxgen, double genkwh, double temp, double mintemp, double maxtemp, double hour, double minute)
+{
+  
+  int MINTEMP = -15;
+  int MAXTEMP = 40;
+  byte imageindex;
+
+  int importing = 0;
+  if (use>gen) importing = 1;
+  double grid = use-gen;
+  
+  glcd.clear();
+  glcd.fillRect(0,0,128,64,0);
+  
+  glcd.drawLine(64, 0, 64, 64, WHITE);      //top vertical line
+  glcd.drawLine(0, 32, 128, 32, WHITE);     //middle horizontal line 
+
+  //variables to store conversion
+  char str[50];
+  char str2[5];
+
+  // Last seen information from EmonTX
+  /*
+  if ((millis()-last_emontx)>20000)
+  {
+    // small font
+    glcd.setFont(font_clR4x6);
+    itoa((millis()-last_emontx)/1000, str, 10);
+    strcat(str,"s TX!");
+    glcd.drawString(3,0,str);
+  }
+  if ((millis()-last_emonbase)>20000)
+  {
+    // small font
+    glcd.setFont(font_clR4x6);
+    itoa((millis()-last_emonbase)/1000, str, 10);
+    strcat(str,"s base!");
+    glcd.drawString(67,34,str);
+  }
+  */
+  
+  // medium font
+  glcd.setFont(font_clR6x8);
+
+  // Indicator for consuming or generating   
+  glcd.drawBitmap(51,34,icon_lines_12x12,16,12,1);
+  if (importing==1) glcd.drawString_P(35,34,PSTR("<<")); else glcd.drawString_P(35,34,PSTR(">>"));
+
+  //big bold font
+  glcd.setFont(font_helvB14);
+
+  // Amount of power currently being used
+  if(use > 1000)
+  {
+    dtostrf(use/1000,2,1,str);
+    strcat(str,"kw");   
+  }
+  else
+  {
+    itoa((int)use,str,10);
+    strcat(str,"w");   
+  }  
+  glcd.drawBitmap(49,0,icon_home_13x12,16,12,1);
+  glcd.drawString(3,9,str);
+
+  // Amount of energy being generated
+  if(gen>1000)
+  {
+    dtostrf(gen/1000,2,1,str);
+    strcat(str,"kw");   
+  }
+  else
+  {
+    itoa((int)gen,str,10);
+    strcat(str,"w");   
+  }
+  if (gen>maxgen) maxgen=gen;
+  imageindex=int(gen/maxgen*5-0.5);
+  glcd.drawBitmap(115,0,icon_solar_12x12[imageindex],16,12,1);
+  glcd.drawString(71,9,str);
+
+  // Amount of energy coming from or going into the grid
+  if(grid<-1000 || grid>1000)
+  {
+    dtostrf(grid/1000,2,1,str);
+    strcat(str,"kw");   
+  }
+  else
+  {
+    itoa((int)grid,str,10);
+    strcat(str,"w");   
+  }
+  glcd.drawString(3,42,str);
+
+  // Temperature               
+  dtostrf(temp,0,1,str); 
+  strcat(str,"c");
+  if (temp>MAXTEMP)
+  {
+    imageindex=5;
+  }
+  else
+  {
+    if (temp<MINTEMP) imageindex=0; else imageindex=int((temp-MINTEMP)/(MAXTEMP-MINTEMP)*5-0.5);
+  }
+  glcd.drawBitmap(120,40,icon_heating_8x16[imageindex],8,16,1);
+  glcd.drawString(70,42,str);   
+
+  glcd.setFont(font_clR4x6);   		//small font - Kwh
+
+  // Kwh consumed today
+  dtostrf(usekwh,0,1,str);
+  strcat(str,"Kwh today");  
+  glcd.drawString(6,26,str);
+
+  // Kwh generated today
+  dtostrf(genkwh,0,1,str); 
+  strcat(str,"Kwh today");  
+  glcd.drawString(71,26,str);
+
+  // Minimum and maximum temperatures
+  itoa((int)mintemp,str,10);
+  strcat(str,"c");
+  glcd.drawString_P(68,58,PSTR("min"));
+  glcd.drawString(82,58,str);
+  itoa((int)maxtemp,str,10); 
+  strcat(str,"c");
+  glcd.drawString_P(97,58,PSTR("max"));
+  glcd.drawString(111,58,str);
+
+  // Current time
+  glcd.drawString_P(5,58,PSTR("Time:"));
+  itoa((int)hour,str,10);
+  if  (minute<10) strcat(str,":0"); else strcat(str,":"); 
+  itoa((int)minute,str2,10);
+  strcat(str,str2); 
+  glcd.drawString(28,58,str); 
+
+}
