1fa1bbe702e17a347e49c8929286b162badf5e69
glynhudson <glyn.hudson@gmail.com>
2013-04-08 12:35:32 +0100

    fix error in last commit

--- SolarPV/SolarPV.ino
+++ SolarPV/SolarPV.ino
@@ -210,7 +210,7 @@ void loop()
       draw_solar_page(cval_use, usekwh, cval_gen, maxgen, genkwh, temp, mintemp, maxtemp, hour,minute, last_emontx, last_emonbase);
       glcd.refresh();
     }
-    else if (page==2)
+    /*else if (page==2)
     {
       draw_power_page( "POWER" ,cval_use, "USE", usekwh);
       draw_temperature_time_footer(temp, mintemp, maxtemp, hour,minute);
@@ -221,11 +221,11 @@ void loop()
       draw_power_page( "SOLAR" ,cval_gen, "GEN", genkwh);
       draw_temperature_time_footer(temp, mintemp, maxtemp, hour,minute);
       glcd.refresh();
+    }*/
+    else if (page==2)
+    {
+      draw_history_page(gen_history, use_history);
     }
-    //else if (page==4)
-    //{
-    //  draw_history_page(gen_history, use_history);
-    //}
 
     int LDR = analogRead(LDRpin);                     // Read the LDR Value so we can work out the light level in the room.
     int LDRbacklight = map(LDR, 0, 1023, 50, 250);    // Map the data from the LDR from 0-1023 (Max seen 1000) to var GLCDbrightness min/max
